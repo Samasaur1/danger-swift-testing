050da8b8b752475e773988c5f2226d996e46bcee
Sam <samgauck@icloud.com>
2019-07-25 15:07:14 -0400

    Check for version in PR title

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,24 +2,39 @@ import Danger
 import Foundation
 let danger = Danger()
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-if editedFiles.contains("CHANGELOG") {
+
+let newVersion: String
+if let range = danger.github.pullRequest.title.range(of: #"(?<=Version )\d+\.\d+\.\d+(?=: .+)"#, options: .regularExpression) {
+    newVersion = String(danger.github.pullRequest.title[range])
+    message("The new version is v\(newVersion)")
+} else {
+    fail("There is no version in the pull request title!")
+    newVersion = "0.0.0"
+}
+
+if danger.github.pullRequest.title.contains("$DESCRIPTION") {
+    fail("$DESCRIPTION placeholder has not been filled in!")
+}
+
+containsChangelog: if editedFiles.contains("CHANGELOG") {
     let lineNumber = danger.utils.lines(for: "## ", inFile: "CHANGELOG")[1]
     let line = danger.utils.readFile("CHANGELOG").split(separator: "\n")[lineNumber - 1]
     let dateString = String(line.drop { $0 != "]" }.dropFirst(4))
     let df = DateFormatter()
     df.dateFormat = "yyyy-MM-dd"
-    let date = df.date(from: dateString) ?? Date.distantPast
-    var c = Calendar.init(identifier: .gregorian)
-    c.timeZone = TimeZone(abbreviation: "GMT-4") ?? c.timeZone
-
-    let changelogDate = c.date(from: c.dateComponents([.year, .month, .day], from: date))!
-    let actualDate = c.date(from: c.dateComponents([.year, .month, .day], from: Date()))!
-
-    if actualDate != changelogDate {
+    let todaysDate = df.string(from: Date())
+    guard df.date(from: dateString) != nil else {
+        fail("Illegal date string in CHANGELOG!")
+        fail(message: "Illegal date string in CHANGELOG!", file: "CHANGELOG", line: lineNumber)
+        suggestion(code: "## [\(newVersion)] - \(todaysDate)", file: "CHANGELOG", line: lineNumber)
+        break containsChangelog
+    }
+    if todaysDate != dateString {
         fail("The latest date in the CHANGELOG is not today!")
         fail(message: "The latest date in the CHANGELOG is not today!", file: "CHANGELOG", line: lineNumber)
+        suggestion(code: "## [\(newVersion)] - \(todaysDate)", file: "CHANGELOG", line: lineNumber)
     } else {
-        message("The latest date in the CHANGELOG is today, \(changelogDate.description)")
+        message("The latest date in the CHANGELOG is today, \(dateString)")
     }
 } else {
     if FileManager.default.fileExists(atPath: "CHANGELOG") {
@@ -28,6 +43,3 @@ if editedFiles.contains("CHANGELOG") {
         warn("There is no CHANGELOG!")
     }
 }
-message("These files have changed: \(editedFiles)")
-
-//danger.github.pullRequest.title
