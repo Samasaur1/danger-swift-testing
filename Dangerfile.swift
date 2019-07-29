87938ec09179aa16b6739d4a5352142de37e52c0
Sam <samgauck@icloud.com>
2019-07-29 14:22:41 -0400

    Actually fix Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -59,7 +59,7 @@ if danger.github.pullRequest.base.ref == "master" {
 for file in editedFiles where file.hasPrefix("Sources/") {
     let dir = file.dropFirst("Sources/".count).split(separator: "/")[0]
     let fileName = file.dropFirst("Sources/\(dir)/".count).dropLast(".swift".count)
-    let testsFile = "Tests/\(dir)/\(fileName)Tests.swift"
+    let testsFile = "Tests/\(dir)Tests/\(fileName)Tests.swift"
     if editedFiles.contains(testsFile) {
         message("\(file) has had corresponding changes in its test file")
     } else {
